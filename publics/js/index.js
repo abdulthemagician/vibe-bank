ed273ce0aa77407316f2d4ffb66db2b9a5ead60d
abdulthemagician <thanapol.suwannamek@hotmail.com>
2026-06-12 16:11:51 +0700

    feat: implement post endpoints and validation for user input

--- publics/js/index.js
+++ publics/js/index.js
@@ -1,4 +1,35 @@
+const transactionDateInput = document.getElementById('transaction-date-input');
+const amountInput = document.getElementById('amount-input');
+const noteInput = document.getElementById('note-input');
+const actionInput = document.getElementById('action-input');
 const API_URL = 'http://localhost:8000';
+
+const validateInput = (transaction) => {
+    let errors = [];
+
+    if(!transaction.transaction_date){
+        errors.push('กรุณาระบุวันที่ทำธุรกรรม');
+    }else if(isNaN(Date.parse(transaction.transaction_date))){
+        errors.push('รูปแบบวันที่ไม่ถูกต้อง');
+    }
+
+    if(!transaction.amount || isNaN(transaction.amount)){
+        errors.push('กรุณากรอกจำนวนเงินเป็นจำนวนเต็มหรือทศนิยม');
+    }else if(transaction.amount <= 0){
+        errors.push('จำนวนเงินต้องมากกว่า 0 บาท');
+    }
+
+    if(!transaction.action_type){
+        errors.push('กรุณาระบุประเภทธุรกรรม');
+    }else if(transaction.action_type !== 'deposit' && transaction.action_type !== 'withdraw'){
+        errors.push('ประเภทธุรกรรมมีแค่ deposit (ฝากเงิน) และ withdraw (ถอนเงิน) เท่านั้น');
+    }
+
+    return {
+        isValid: errors.length === 0,
+        errorMessage: errors
+    };
+}
 
 const timeOption = {
     year: 'numeric',
@@ -20,26 +51,12 @@ const getCurrentDate = () => {
     return `${now.year}-${now.month}-${now.day}T${now.hour}:${now.minute}`
 }
 
-const transactionDateInput = document.getElementById('transaction-date-input');
-const amountInput = document.getElementById('amount-input');
-const noteInput = document.getElementById('note-input');
-const actionInput = document.getElementById('action-input');
-transactionDateInput.value = getCurrentDate();
-
-const clearBtn = document.getElementById('clear-btn');
-clearBtn.addEventListener('click', (event) => {
-    transactionDateInput.value = getCurrentDate();
-    amountInput.value = '';
-    noteInput.value = '';
-    actionInput.value = 'deposit';
-});
-
+const tableBody = document.getElementById('table-body');
 const fetchTransaction = async () => {
     try{
-        const tableBody = document.getElementById('table-body');
         const response = await axios.get(`${API_URL}/transaction/`);
         const transactions = response.data;
-        // tableBody.innerHTML = '';
+        tableBody.innerHTML = '';
         transactions.forEach((transaction) => {
             const tableRow = document.createElement('tr');
             const transaction_date = document.createElement('td');
@@ -60,4 +77,69 @@ const fetchTransaction = async () => {
     }
 };
 
+const form = document.querySelector('form');
+const statusAlert = document.getElementById('status-alert');
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    try{
+        const transactionDate = transactionDateInput.value;
+        const amount = amountInput.value;
+        const action_type = actionInput.value;
+        const note = noteInput.value;
+
+        const transactionRow = {
+            'transaction_date': transactionDate,
+            'amount': amount,
+            'action_type': action_type,
+            'note': note
+        }
+
+        const errors = validateInput(transactionRow);
+        if(!errors.isValid){
+            const validationError = new Error('กรอกข้อมูลไม่ครบหรือไม่ถูกต้อง โปรดตรวจสอบอีกครั้ง');
+            validationError.customErrors = errors.errorMessage;
+            throw validationError;
+        }
+
+        const response = await axios.post(`${API_URL}/transaction/`, transactionRow);
+        statusAlert.textContent = 'บันทึกข้อมูลเรียบร้อย';
+        statusAlert.classList.remove('alert-danger');
+        statusAlert.classList.add('d-flex', 'alert', 'alert-success');
+        form.reset();
+        transactionDateInput.value = getCurrentDate();
+    }catch(error){
+        if(error.response){
+            const statusCode = error.response.data.status;
+            error.message = error.response.data.message;
+            error.customErrors = error.response.data.errors;
+        }else if(error.request){
+            error.message = 'ไม่สามารถเชื่อมต่อ Server ได้ กรุณาลองใหม่อีกครั้ง';
+        }
+
+        if(error.customErrors && error.customErrors.length > 0){
+            const listErrors = error.customErrors.map(errMsg => `<li>${errMsg}</li>`).join('');
+            statusAlert.innerHTML = `<div class="mb-1" style="font-size: 1.1rem"><b>${error.message}</b></B></div>`;
+            statusAlert.innerHTML += `<ul class="mb-0 ps-3">${listErrors}</ul>`;
+
+        }
+        statusAlert.classList.remove('alert-success');
+        statusAlert.classList.add('d-flex', 'flex-column', 'alert', 'alert-danger', 'text-start');
+        console.log(error.message);
+        console.log(error.customErrors);
+    }
+});
+
+const clearBtn = document.getElementById('clear-btn');
+clearBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    transactionDateInput.value = getCurrentDate();
+    amountInput.value = '';
+    noteInput.value = '';
+    actionInput.value = 'deposit';
+
+    statusAlert.textContent = '';
+    statusAlert.classList.remove('d-flex', 'alert', 'alert-success', 'alert-danger');
+});
+
+transactionDateInput.value = getCurrentDate();
 fetchTransaction();
